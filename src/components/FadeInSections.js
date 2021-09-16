26cecd8c2c43c0e8740f4d166253b932058784d0
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-09-16 18:27:03 -0400

    Fixed CSS, Fadein Animations

--- src/components/FadeInSections.js
+++ src/components/FadeInSections.js
@@ -4,12 +4,38 @@ const FadeInSections = (props) => {
   const [isVisible, setVisible] = useState(null);
   const domRef = React.useRef();
 
+  //Uses intersectionObserver to show when child element is on screen, doesn't reset on scroll up or scroll down. !!!!! Eventually want it to reset on scroll down !!!!!
+
   useEffect(() => {
+    let prevRatio;
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
+      entries.forEach((entry) => {
+        if (entry.intersectionRatio > prevRatio) {
+          setVisible(entry.isIntersecting);
+        }
+        prevRatio = entry.intersectionRatio;
+      });
     });
     observer.observe(domRef.current);
   }, []);
+
+  //!!!!!!!!!!!!Still under development!!!!!!!!!!!!!!!! show visible css if prev scroll position is less than current scroll position. But right now FadeInSections uses prop.children and useRef, which doesn't work well with window.scrollY
+
+  // const scrollFunc = () => {
+  //   if(prevPos <= visiblePos){
+  //    setVisiblePos(window.scrollY);
+  //   } else if (prevPos > visiblePos) {
+  //     setVisible(true);
+  //     console.log("true");
+  //   }
+  //   setPrevPos(visiblePos);
+  // };
+
+  // const debounceScroll = useMemo(() => debounce(scrollFunc, 300), []);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", debounceScroll);
+  // }, []);
 
   return (
     <div
