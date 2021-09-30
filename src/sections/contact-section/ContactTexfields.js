df335442c84cb24e14f9cd080dd2768876692dab
mfunakoshi <masayafunakoshi1@gmail.com>
2021-09-29 22:11:33 -0400

    Fixing Favicon

--- src/sections/contact-section/ContactTexfields.js
+++ src/sections/contact-section/ContactTexfields.js
@@ -5,7 +5,7 @@ import "./Contact.css";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import emailjs from "emailjs-com";
+import emailjs from 'emailjs-com';
 
 const ContactTexfields = () => {
   const [contactData, setContactData] = useState({
