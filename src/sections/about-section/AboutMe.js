705260b8faa321856eb9a6fcdd1a1c9fc1de7509
masayafunakoshi1 <masayafunakoshi1@gmail.com>
2021-11-23 00:53:17 -0500

    Changes font styling, edited aboutme text

--- src/sections/about-section/AboutMe.js
+++ src/sections/about-section/AboutMe.js
@@ -30,41 +30,40 @@ const AboutMe = () => {
           <span className="AboutMe-Text">
             <FadeInSections>
               Hey there! My name's Masaya Funakoshi, 22-years-old, born and
-              raised in Tarrytown, NY. Currently specializing in Front-End Web
+              raised in Tarrytown, NY. Experienced with Front-End Web
               Development, primarily focusing on HTML, CSS, JS and React.
-              Completely self-taught for the past 9-months and going strong till
-              I find my first dev role!
+              Currently working on gaining a deeper understanding of
+              Object-Oriented-Programming, Design Patterns, and clean code.
               <p>
-                I graduated in the Spring of 2020 with a BA in Arts
-                Management... right at the peak of the pandemic 😅. The Arts
-                took quite a hit from the quarantine, so with my lack of
-                jobs/internships and newly found time, I actually decided to do
-                something productive and learned how to code! And of course, you
-                know how the story goes... I fell in love with it 😍
+                I graduated in the Spring of 2020 with a BA in Arts Management,
+                right at the peak of the pandemic. The Arts took quite a hit
+                from the quarantine, leaving me with no internships and alot of
+                time. With this new-found freedom, I decided to do something
+                I've always wanted to do, which was learn how to code. And of
+                course, you know how the story goes... I fell in love with it.
               </p>
               <p>
-                I always thought I was too dumb to learn how to code, and saying
-                I wasn't good at Math or Science is putting it lightly. BUT
-                despite all that, a friend inspired me try a website called{" "}
+                I believe everyone has that "Aha!" moment when they realize
+                their passion for coding. Mine was when I wrote my first
+                console.log("Hello World!") in JavaScript. Seeing my words
+                magically appear on the console BLEW MY MIND. The machine was
+                TALKING TO ME. Or maybe it was more me... talking to myself?
+                Anyways, it made me feel like wrinkly Emperor Palpatine in this
+                Star Wars
                 <a
-                  className="AboutMe-Link-Text"
-                  href="https://www.internetingishard.com/"
+                  href="https://www.youtube.com/watch?v=Sg14jNbBb-8"
+                  alt="Emperor Palpatine Unlimited Power scene"
                   target="_blank"
-                  rel="noopener noreferrer"
+                  className="AboutMe-Link-Text"
                 >
-                  Interneting Is Hard{" "}
+                  &#160;scene.
                 </a>
-                , teaching me the basics of HTML and CSS. It wasn't a "Hello
-                World!" for me, no, it was when I was able to change the size
-                and color of a blue HTML &#60;div&#62; to red with a seperate
-                CSS doc. It blew my mind. I was hooked. I had to know more.
               </p>
               <p>
                 Throughout this journey, it's been a series of ups and downs.
                 The euphoria you feel when you solve an issue that's been
                 bugging you for days, as well as the frustration of a small
-                problem leading you down a rabbit hole, eventually creating even
-                more problems than you thought possible. At the end of the day,
+                problem leading you down a rabbit hole. At the end of the day,
                 it's taught me a mindset, any problem can be solved with enough
                 perserverance, logical thinking, and Google. I appreciate the
                 experiences I've had so far, and am looking forward to
